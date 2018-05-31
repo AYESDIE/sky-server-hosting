6cecfc418135534fd1a43daa52d7b9a486a53d26
Ayush Chamoli <34449856+AYESDIE@users.noreply.github.com>
2018-05-31 11:29:20 +0530

    Add files via upload

--- sky.js
+++ sky.js
@@ -1,29 +1,20 @@
 var express     = require("express"),
-    app         = express(),
-    spawn       = require('child_process').spawn,
-    py          = spawn('python', ['moon/moon.py']),
-    data        = ["http://www.gstatic.com/tv/thumb/persons/835149/835149_v9_bb.jpg"],
-    dataString  = '';
+    app         = express();
 
+app.set("view engine", "ejs");
+app.get("/", function(req, res){
+    res.render("landing");
+});
 
-app.get("/", function(req,res) {
-    py.stdout.on('data', function(data){
-        dataString += data.toString();
-        console.log(dataString);
-    });
-    py.stdout.on('end', function(){
-        console.log('{FACE: ',dataString,'}');
-    });
-    py.stdin.write(JSON.stringify(data));
-    py.stdin.end();
-    res.send("ok");
+app.get("/face", function(req, res){
+    res.render("new");
 });
 
 
 app.get("*", function(req,res) {
-    res.send("stop it , get some help!");
+    res.redirect("/");
 });
 
-app.listen(3000, function() {
-    console.log("SERVER IS UP at http://localhost:3000/");
+app.listen(8080, function() {
+    console.log("SERVER IS UP at http://localhost:8080/");
 } );
